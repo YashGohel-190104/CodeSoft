696a11002752de8e51cc7aa008e9728ceec9d8c3
YashGohel-190104 <gohelyash1901@gmail.com>
2024-06-28 23:28:08 +0530

    First_Page_Done

---
+++ PortFolio.js
@@ -0,0 +1,8 @@
+// @ts-ignore
+var typed= new Typed("#text", {
+    strings:["FullStackDeveloper","Android App Developer" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
